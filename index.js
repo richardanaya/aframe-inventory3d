d67039e889a094c6dd2aafbf636f47733dd7a370
Richard <richard.anaya@gmail.com>
2016-06-11 19:37:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+// aframe-inventory3d.js
+// repo    : https://github.com/richardanaya/aframe-inventory3d
+// license : MIT
+
+(function (window, module, AFRAME) {
+	"use strict";
+	AFRAME = AFRAME.aframeCore || AFRAME;
+
+	AFRAME.registerComponent('inventory-3d', {
+		schema: {type: 'string'},
+
+		configureObject: function(obj,configuration){
+			configuration.materials.forEach(function(mat,i){
+				if(mat.index==undefined){
+					for(var j = 0 ; j < obj.children[0].geometry.faces.length; j++ ){
+						var f = obj.children[0].geometry.faces[j];
+						if(f.daeMaterial==mat.id){
+							mat.index = f.materialIndex;
+							break;
+						}
+					}
+				}
+				obj.children[0].material.materials[mat.index] = new THREE.MeshBasicMaterial( { color: mat.color } );
+			});
+		},
+
+		loadModel: function(configurations,url){
+			var _this = this;
+			var modelEl = document.createElement("a-entity");
+			modelEl.setAttribute("collada-model", "url("+url+")");
+			modelEl.addEventListener("model-loaded", function (e) {
+				function findColladaObjects(o, objs) {
+					if (!o) {
+						return
+					}
+					if (o.colladaId) {
+						objs.push({id: o.colladaId, object3D: o})
+					}
+					if (o.children) {
+						o.children.forEach(function (child) {
+							findColladaObjects(child, objs)
+						});
+					}
+				}
+
+				var colladaObjs = [];
+				findColladaObjects(modelEl.object3D, colladaObjs);
+				colladaObjs.forEach(function(co){
+					var id = co.id;
+					var configuration = null;
+					configurations.objects.forEach(function(config){
+						if(config.id==id){
+							configuration = config;
+						}
+					})
+					if(configuration){
+						_this.configureObject(co.object3D,configuration);
+					}
+				})
+			})
+			_this.el.appendChild(modelEl);
+		},
+
+		attributeChangedCallback: {
+			value: function (attr, oldVal, newVal) {
+				debugger
+			}
+		},
+
+		loadInventory3D: function (json) {
+			this.el.innerHTML = "";
+			var _this = this;
+			json.scenes.forEach(function (scene) {
+				_this.loadModel(scene.configuration, scene.url)
+			})
+		},
+
+		update: function () {
+			var _this = this;
+			_this.el.innerHTML = "";
+			window.fetch(this.data)
+				.then(function (response) {
+					return response.json()
+				}).then(function (json) {
+					_this.loadInventory3D(json);
+				}).catch(function (ex) {
+					console.log('parsing failed', ex)
+				})
+		}
+	});
+})(
+	typeof window !== "undefined" ? window : {},
+	typeof module !== "undefined" ? module : {},
+	typeof require !== "undefined" ? require("aframe") : (AFRAME || window.AFRAME)
+);
